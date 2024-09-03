bfe7686d6ca9791b243514bf1925297be7073c5e
CHRISTOPHER GOLD <cgoldaz@gmail.com>
2024-09-03 23:09:00 +0800

    maybe a fix

--- src/userBalance.js
+++ src/userBalance.js
@@ -1,14 +1,17 @@
 const { JsonRpcProvider, Wallet, formatEther } = require("ethers");
 
 const {
-    PRIVATE_KEY,
     RPC_URL
 } = require("./config");
 
 const provider = new JsonRpcProvider(RPC_URL);
 
 async function getUserBalance({ address }) {
-    const balance = await provider.getBalance(address);
+    const balance = await provider.getBalance(
+        {
+            address: address
+        }
+    );
     return parseFloat(formatEther(balance));
 }
 
