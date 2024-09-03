18ed0ef790ebecdd95c3b9158ab2c6c015a58066
CHRISTOPHER GOLD <cgoldaz@gmail.com>
2024-09-03 23:03:21 +0800

    whoops...

--- src/balance.js
+++ src/balance.js
@@ -1,13 +1,15 @@
-const { JsonRpcProvider, formatEther } = require("ethers");
+const { JsonRpcProvider, Wallet, formatEther } = require("ethers");
 
 const {
+    PRIVATE_KEY,
     RPC_URL
 } = require("./config");
 
-const provider = new JsonRpcProvider(RPC_URL);;
+const provider = new JsonRpcProvider(RPC_URL);
+const wallet = new Wallet(PRIVATE_KEY, provider);
 
 async function Balance() {
-    const balance = await provider.getBalance(address);
+    const balance = await provider.getBalance(wallet.address);
     return parseFloat(formatEther(balance));
 }
 
